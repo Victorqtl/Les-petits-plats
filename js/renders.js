8bba5fe94fd3def86616a6062963d97117320ff5
Victor Quertelet <victor.quertelet@gmail.com>
2024-08-06 09:43:17 -0400

    Changement du chemin pour l'importation des images de recettes

--- js/renders.js
+++ js/renders.js
@@ -4,7 +4,7 @@ function renderRecipes(recipes) {
     `
     <div class="recipe w-96 flex flex-col bg-white rounded-[20px]">
       <div class="relative">
-        <img class="w-full h-64 object-cover rounded-t-[20px]" src="../photos/${recipe.image}" alt="Photo du plat"></img>
+        <img class="w-full h-64 object-cover rounded-t-[20px]" src="./photos/${recipe.image}" alt="Photo du plat"></img>
         <p class="absolute top-5 right-5 px-3.5 py-1.5 text-xs font-manrope bg-custom-yellow rounded-[20px]">${recipe.time}min</p>
       </div>
       <h2 class="mt-8 px-6 font-anton text-lg">${recipe.name}</h2>
